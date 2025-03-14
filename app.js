70b3150c63cc0b66d37c3edf90da9278e96191b4
Jose Mendoza <joseemz@outlook.es>
2025-03-14 08:36:52 -0600

    actualizar array amigos y limpiar entrada

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
-let amigos = []; //Variable array vacia
+let amigos = [];   //Variable con array vacia
 
 function agregarAmigo () {
-    let entrada = document.getElementById("amigo");
-    let nombre = entrada.value.trim();
-    if (nombre === "") {
-        alert("Por favor, inserte un nombre.");
-
+    let entrada = document.getElementById("amigo");  //Variable que usa la entrada.
+    let nombre = entrada.value.trim();  //Variable que tiene el valor de "entrada" y quita los espacios con trim.
+    let lista = document.getElementById("listaAmigos");  //Variable que usa la lista de amigos.
+    if (nombre === "") {  //Compara valor y tipo de dato.
+        alert("Por favor, inserte un nombre.");  //Mensaje de error.
+        return;
     }
+
+    amigos.push(nombre);  //Agrega un nombre al final del array (amigos)
+    entrada.value = "";  //Limpia el valor de la entrada 
+
+    console.log(amigos);
 }
+
+
+
 
 
 
