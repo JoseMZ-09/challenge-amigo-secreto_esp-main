821140db87898c48a0d86b24e9234f87f07f452f
Jose Mendoza <joseemz@outlook.es>
2025-03-14 08:10:35 -0600

    captura valor de entrada y validacion con mensaje de error

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
 let amigos = []; //Variable array vacia
+
+function agregarAmigo () {
+    let entrada = document.getElementById("amigo");
+    let nombre = entrada.value.trim();
+    if (nombre === "") {
+        alert("Por favor, inserte un nombre.");
+
+    }
+}
+
+
+
+
+
+
